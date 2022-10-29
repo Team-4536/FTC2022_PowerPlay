f8a5d14699c7f1cceb11e58d02a49cedc990f322
MinuteBots <97267316+M4rcusBC@users.noreply.github.com>
2022-10-29 10:57:06 -0500

    ITS ALIVE

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Data/ArmData.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.util.Data;
+
+import androidx.annotation.NonNull;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ArmData {
+
+    public final @NonNull DcMotor liftMotor;
+    public final @NonNull Servo gripServo;
+
+    public ArmData(HardwareMap map) {
+        this.liftMotor = map.get(DcMotor.class, "lift_motor");
+        this.liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.gripServo = map.get(Servo.class, "grip_servo");
+    }
+
+}
