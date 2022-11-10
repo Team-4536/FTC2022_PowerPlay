0c676653e5870b41b7f6bcae8ea2bc30591c18eb
Michael-Foley <mfoley001@stpaul.k12.mn.us>
2022-11-10 16:05:41 -0600

    Working Open CV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/autos/OpenCVCameraDetection.java
@@ -0,0 +1,175 @@
+package org.firstinspires.ftc.teamcode.opModes.autos;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.util.Data.AprilTagDetectionPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.functions.DriveFunctions;
+import org.firstinspires.ftc.teamcode.functions.NavFunctions;
+import org.firstinspires.ftc.teamcode.functions.PIDFunctions;
+import org.firstinspires.ftc.teamcode.functions.SequencerFunctions;
+import org.firstinspires.ftc.teamcode.functions.TelemetryFunctions;
+import org.firstinspires.ftc.teamcode.util.Constants;
+import org.firstinspires.ftc.teamcode.util.Data.DriveData;
+import org.firstinspires.ftc.teamcode.util.Data.NavData;
+import org.firstinspires.ftc.teamcode.util.Data.ObjectDetectionData;
+import org.firstinspires.ftc.teamcode.util.Data.PIDData;
+import org.firstinspires.ftc.teamcode.util.Data.TelemetryData;
+import org.firstinspires.ftc.teamcode.util.Step;
+import org.firstinspires.ftc.teamcode.util.V2f;
+
+
+import java.util.ArrayList;
+
+//https://github.com/OpenFTC/EOCV-AprilTag-Plugin
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="OpenCV", group="Autos")
+public class OpenCVCameraDetection extends LinearOpMode
+{
+        OpenCvCamera camera;
+        AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+        static final double FEET_PER_METER = 3.28084;
+
+        // Lens intrinsics
+        // UNITS ARE PIXELS
+        // NOTE: this calibration is for the C920 webcam at 800x448.
+        // You will need to do your own calibration for other configurations!
+        double fx = 578.272;
+        double fy = 578.272;
+        double cx = 402.145;
+        double cy = 221.506;
+
+        // UNITS ARE METERS
+        double tagsize = 0.166;
+
+        int ID_TAG_OF_INTEREST = 1; // Tag ID 18 from the 36h11 family
+
+        int foundTag = -1;
+
+        AprilTagDetection tagOfInterest = null;
+
+        @Override
+        public void runOpMode() {
+                TelemetryData telemetry = new TelemetryData("State");
+
+                NavData nav = new NavData(this.hardwareMap);
+
+                DriveData drive = new DriveData(
+                        new boolean[]{true, false, true, false},
+                        this.hardwareMap);
+                PIDData drivePID = new PIDData(0.018f, 0.0f, -0.2f);
+
+
+                Step[][] autos = new Step[][]{
+
+                        new Step[]{},
+
+                        new Step[]{
+                                new Step(new float[]{-0.4f, 0f}, 2),
+                                new Step(new float[]{0, 0.4f}, 2)
+                        },
+
+                        new Step[]{
+                                new Step(new float[]{0, 0.4f}, 2),
+                        },
+
+                        new Step[]{
+                                new Step(new float[]{0.4f, 0f}, 2),
+                                new Step(new float[]{0, 0.4f}, 2)
+                        }
+                };
+
+                int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+                aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+                camera.setPipeline(aprilTagDetectionPipeline);
+                camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                        @Override
+                        public void onOpened() {
+                                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+                        }
+
+                        @Override
+                        public void onError(int errorCode) {
+
+                        }
+                });
+
+                telemetry.addChild("Initialized!", "");
+                TelemetryFunctions.sendTelemetry(this.telemetry, telemetry);
+                waitForStart();
+                nav.timer.reset();
+
+
+
+
+                while (opModeIsActive()) {
+
+                        NavFunctions.updateDt(nav);
+                        NavFunctions.updateHeading(nav);
+
+                        float PIDOut = PIDFunctions.updatePIDAngular(
+                                drivePID,
+                                nav.heading,
+                                (float) nav.dt);
+
+                        ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+                        if (foundTag == -1) {
+                                telemetry.addChild("length", currentDetections.size());
+                                if (currentDetections.size() != 0) {
+
+                                        for (AprilTagDetection tag : currentDetections) {
+                                                foundTag = tag.id;
+                                                telemetry.addChild("Tag Found:", foundTag);
+                                                TelemetryFunctions.sendTelemetry(this.telemetry, telemetry);
+                                                break;
+
+                                        }
+                                }
+                        } else {
+                                telemetry.addChild("Tag Found:", foundTag);
+                                //telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+                                //telemetry.addChild("\nDetected tag ID=%d", foundTag);
+                                TelemetryFunctions.sendTelemetry(this.telemetry, telemetry);
+                                if (foundTag == 0) {
+                                        telemetry.addChild("Tag Found:", foundTag);
+                                }
+                                if (foundTag == 1) {
+                                        telemetry.addChild("Tag Found:", foundTag);
+                                }
+                                if (foundTag == 2) {
+                                        telemetry.addChild("Tag Found:", foundTag);
+                                }
+
+                                TelemetryFunctions.sendTelemetry(this.telemetry, telemetry);
+                        }
+
+
+                }
+
+                /*
+                 * The START command just came in: now work off the latest snapshot acquired
+                 * during the init loop.
+                 */
+
+                /* Update the telemetry */
+
+
+
+
+                /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+
+
+        }
+}
